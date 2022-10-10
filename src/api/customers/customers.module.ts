23949f5411d93916ef0ab25c454f75aa64b34ac0
Jairo Martinez <ocrensys@gmail.com>
2022-10-10 13:55:11 -0600

    Move RoleGuard to API Module

--- src/api/customers/customers.module.ts
+++ src/api/customers/customers.module.ts
@@ -3,18 +3,10 @@ import { CustomersService } from './customers.service';
 import { CustomersController } from './customers.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Customer } from './entities/customer.entity';
-import { APP_GUARD } from '@nestjs/core';
-import { RoleGuard } from 'src/guards/role/role.guard';
 
 @Module({
   controllers: [CustomersController],
-  providers: [
-    CustomersService,
-    {
-      provide: APP_GUARD,
-      useClass: RoleGuard,
-    },
-  ],
+  providers: [CustomersService],
   imports: [TypeOrmModule.forFeature([Customer])],
 })
 export class CustomersModule {}
