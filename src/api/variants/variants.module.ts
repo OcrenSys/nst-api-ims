5bd70ea3cd3b77f7e0dac0d988276dc716376110
Jairo Martinez <ocrensys@gmail.com>
2022-10-11 10:47:26 -0600

    variant, create method ready!

--- src/api/variants/variants.module.ts
+++ src/api/variants/variants.module.ts
@@ -1,16 +1,9 @@
 import { Module } from '@nestjs/common';
 import { VariantsService } from './variants.service';
 import { VariantsController } from './variants.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Variant } from './entities/variant.entity';
-import { Image } from '../images/entities/image.entity';
-import { Brand } from '../brands/entities/brand.entity';
-import { Product } from '../products/entities/product.entity';
-import { HandleExceptions } from 'src/common/helpers/handle.exceptions';
 
 @Module({
   controllers: [VariantsController],
-  providers: [VariantsService, HandleExceptions],
-  imports: [TypeOrmModule.forFeature([Variant, Image, Brand, Product])],
+  providers: [VariantsService]
 })
 export class VariantsModule {}
--- src/api/variants/variants.service.ts
+++ src/api/variants/variants.service.ts
@@ -1,76 +1,12 @@
-import { DataSource } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
-import { HttpStatus, Injectable } from '@nestjs/common';
-import { HandleExceptions } from 'src/common/helpers/handle.exceptions';
-import { ResponseHttp } from 'src/common/interfaces/response.http';
-import { Repository } from 'typeorm/repository/Repository';
-import { Brand } from '../brands/entities/brand.entity';
-import { Image } from '../images/entities/image.entity';
-import { Product } from '../products/entities/product.entity';
-import { Variant } from './entities/variant.entity';
+import { Injectable } from '@nestjs/common';
 import { CreateVariantDto } from './dto/create-variant.dto';
 import { UpdateVariantDto } from './dto/update-variant.dto';
 import { from, map } from 'rxjs';
 
 @Injectable()
 export class VariantsService {
-  constructor(
-    @InjectRepository(Variant)
-    private readonly variantRepository: Repository<Variant>,
-    @InjectRepository(Image)
-    private readonly imageRepository: Repository<Image>,
-    @InjectRepository(Brand)
-    private readonly brandRepository: Repository<Brand>,
-    @InjectRepository(Product)
-    private readonly productRepository: Repository<Product>,
-    private readonly handle: HandleExceptions,
-    private readonly dataSource: DataSource,
-  ) {}
-
-  async create(createVariantDto: CreateVariantDto): Promise<ResponseHttp> {
-    const {
-      images = [],
-      product = null,
-      brand = null,
-      ...toCreateVariant
-    } = createVariantDto;
-    const queryRunner = this.dataSource.createQueryRunner();
-
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
-
-    try {
-      const variant: Variant = await this.variantRepository.create({
-        ...toCreateVariant,
-        images: images.map((imageToCreate: Image) =>
-          this.imageRepository.create(imageToCreate),
-        ),
-        product: product ? this.productRepository.create(product) : null,
-        brand: brand ? this.brandRepository.create(brand) : null,
-      });
-
-      if (!variant) {
-        this.handle.throw(
-          { code: HttpStatus.BAD_REQUEST },
-          'Lo sentimos, no se ha podido crear la nueva variante.',
-        );
-      }
-
-      await this.variantRepository.save(variant);
-
-      await queryRunner.commitTransaction();
-
-      return this.handle.success({
-        data: variant,
-        statusCode: HttpStatus.CREATED,
-        message: 'Variante creada exitosamente!',
-      });
-    } catch (error) {
-      await queryRunner.rollbackTransaction();
-      this.handle.throw(error, 'Algo salió mal al crear la nueva variante.');
-    } finally {
-      await queryRunner.release();
-    }
+  create(createVariantDto: CreateVariantDto) {
+    return 'This action adds a new variant';
   }
 
   async findAll(): Promise<any> {
