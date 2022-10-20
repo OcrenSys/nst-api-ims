88e02c59b88e270ac648179161c417e108eb7f0a
Jairo Martinez <ocrensys@gmail.com>
2022-10-20 11:38:24 -0600

    Percent, CRUD ready!

--- src/api/percents/dto/create-percent.dto.ts
+++ src/api/percents/dto/create-percent.dto.ts
@@ -1,1 +1,14 @@
-export class CreatePercentDto {}
+import { IsDecimal, IsInt, IsObject, IsOptional } from 'class-validator';
+import { Credit } from 'src/api/credits/entities/credit.entity';
+
+export class CreatePercentDto {
+  @IsInt()
+  fee: number;
+
+  @IsDecimal()
+  rate: number;
+
+  @IsObject()
+  @IsOptional()
+  credits?: Credit[];
+}
