e36268793dfca62ee4491a16633a8785896aff29
Jairo Martinez <ocrensys@gmail.com>
2022-10-20 10:10:43 -0600

    Payment Date, CRUD ready!

--- src/api/payment-dates/dto/create-payment-date.dto.ts
+++ src/api/payment-dates/dto/create-payment-date.dto.ts
@@ -1,1 +1,24 @@
-export class CreatePaymentDateDto {}
+import { IsDate, IsInt, IsObject, Max, Min } from 'class-validator';
+import { Credit } from 'src/api/credits/entities/credit.entity';
+import { Payment } from 'src/api/payments/entities/payment.entity';
+import * as NUMBER from '../../../common/constants/numbers.constants';
+
+export class CreatePaymentDateDto {
+  @IsInt()
+  @Min(NUMBER.MIN, {
+    message: `El monto de la fecha de pago debe ser un numero entero mayor o igual a ${NUMBER.MIN}`,
+  })
+  @Max(NUMBER.MAX, {
+    message: `El monto de la fecha de pago debe ser un monto real, menor a ${NUMBER.MAX}`,
+  })
+  amount: number;
+
+  @IsDate()
+  date: string;
+
+  @IsObject()
+  credit: Credit;
+
+  @IsObject()
+  payment: Payment;
+}
