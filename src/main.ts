7af6c5057322c80f6b73628b793acb47fcb9423e
Jairo Martinez <ocrensys@gmail.com>
2022-11-07 15:00:29 -0600

    add ssh certificate to project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import fs from 'fs';
+
+const keyPath = './src/common/secrets/key.pem';
+const cerPath = './src/common/secrets/cert.pem';
+const options: any = {};
+
+if (fs.existsSync(keyPath) && fs.existsSync(cerPath)) {
+  options.httpsOptions = {
+    cert: fs.readFileSync(keyPath),
+    key: fs.readFileSync(cerPath),
+  };
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(cerPath),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+  app.enableCors();
   app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
