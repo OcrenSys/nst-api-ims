67063f24e35f2845e86ed878916eb812654211e4
Jairo Martinez <ocrensys@gmail.com>
2023-08-27 02:26:59 -0600

    improving validators methods

--- src/common/helpers/validators.ts
+++ src/common/helpers/validators.ts
@@ -1,10 +1,49 @@
-export function setOptionsWhere(params: unknown): any {
+import { Like } from 'typeorm';
+
+export function setOptionsRelation(parameters: unknown): any {
+  return JSON.parse(parameters as string);
+}
+
+export function setOptionsWhere(parameters: unknown): any {
+  if (
+    typeof parameters === 'string' &&
+    parameters.includes('{') &&
+    parameters.includes('}')
+  ) {
+    parameters = JSON.parse(parameters);
+  }
+
   let options = {};
-  Object.entries(params).forEach(([key, value]: [string, any]) => {
-    options = {
-      ...options,
-      [key]: JSON.parse(value),
-    };
-  });
+  if (typeof parameters === 'object') {
+    Object.entries(parameters).forEach(
+      ([key, value]: [
+        string,
+        string | number | boolean | object | unknown,
+      ]) => {
+        options = {
+          ...options,
+          [key]: getValue(value),
+        };
+      },
+    );
+  }
+
   return options;
+}
+
+function getValue(
+  value: string | number | boolean | object | unknown,
+): string | number | boolean | object | unknown {
+  if (typeof value === 'string' && value.includes('{') && value.includes('}')) {
+    value = JSON.parse(value);
+  }
+
+  const values = {
+    object: setOptionsWhere(value),
+    string: Like(`%${value || ''}%`),
+    number: +value,
+    boolean: value,
+  };
+
+  return values[typeof value] || value;
 }
