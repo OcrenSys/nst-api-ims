05715146b8f7faf175912baf6ac89f5b1865a5ac
Jairo Martinez <ocrensys@gmail.com>
2023-09-26 23:06:36 -0600

    refactor env

--- src/services/authentication/authentication.service.ts
+++ src/services/authentication/authentication.service.ts
@@ -2,35 +2,15 @@ import { Injectable } from '@nestjs/common';
 import * as firebase from 'firebase-admin';
 import { ServiceAccount } from 'firebase-admin';
 
-type TServiceAccount = {
-  type?: string;
-  project_id?: string;
-  private_key_id?: string;
-  private_key?: string;
-  client_email?: string;
-  client_id?: string;
-  auth_uri?: string;
-  token_uri?: string;
-  auth_provider_x509_cert_url?: string;
-  client_x509_cert_url?: string;
-  universe_domain?: string;
-};
-
 @Injectable()
 export class AuthenticationService {
   private firebaseApp: firebase.app.App;
 
   constructor() {
-    const {
-      private_key = '',
-      client_email = '',
-      project_id = '',
-    }: TServiceAccount = process.env.SERVICE_ACCOUNT as TServiceAccount;
-
     const serviceAccount: ServiceAccount = {
-      projectId: project_id,
-      clientEmail: client_email,
-      privateKey: private_key,
+      projectId: process.env.SERVICE_ACCOUNT_PROJECT_ID,
+      clientEmail: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+      privateKey: process.env.SERVICE_ACCOUNT_PRIVATE_KEY,
     };
 
     this.firebaseApp = firebase.initializeApp({
